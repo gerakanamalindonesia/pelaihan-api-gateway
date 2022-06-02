06e5dd0bb49401764e7fafbcda1b575351252ee2
gerakanamalindonesia <gerakanamalindonesia@gmail.com>
2022-06-02 23:43:19 +0700

    membuat dan mengelompokkan apiname ke file json

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const axios = require("axios");
+const registry = require("./registry.json");
 
-router.all("/:apiName", (req, res) => {
+router.all("/:apiName/:path", (req, res) => {
   console.log(req.params.apiName);
-  axios.get("http://localhost:4001/" + req.params.apiName).then((response) => {
-    res.send(response.data);
-  });
+  if (registry.services[req.params.apiName]) {
+    axios
+      .get(registry.services[req.params.apiName].url + req.params.path)
+      .then((response) => {
+        res.send(response.data);
+      });
+  } else {
+    res.send("Api name tidak ditemukan");
+  }
 });
 
 module.exports = router;
